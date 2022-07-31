115461d3ca5de00953a22db428ad2789054bee4b
UtkarshKumar088 <utkarshjarial08@gmail.com>
2022-08-01 03:27:01 +0530

    added static

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('api/',include('base.api.urls')),
    
 
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
-urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
